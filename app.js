621f3edfa6d691dcfdef44149023e2be9341179c
FinnakaLite <Finng2008@outlook.com>
2025-09-02 13:22:56 +0200

    Oppgave 1 Ferdig!

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+let usrname = "Stranger";
 
+function oppgaveEn() {
+    // Tar inn navnet, og lager en pop up med hei navnet
+    usrname = prompt("Hva er navnet ditt?");
+    alert("Hei " + usrname);
+};
